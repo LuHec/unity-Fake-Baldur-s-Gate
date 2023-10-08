2ccee6805b1997ea2f511e217ff796671dbd337f
LuHec <92776720+LuHec@users.noreply.github.com>
2023-10-09 01:40:12 +0800

    update

---
+++ Assets/Project/Scripts/Manager/ICenter.cs
@@ -0,0 +1,4 @@
+﻿public interface ICenter
+{
+    void CenterUpdate();
+}
---
+++ Assets/Project/Scripts/Manager/Map/MapSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 全局地图管理器
+/// </summary>
+public class MapSystem : ICenter
+{
+    private GridXZ<GridObject> _grid;
+    private int _gridwidth;
+    private int _gridheight;
+    private float _cellsize;
+    private Vector3 _originPos;
+    
+    public MapSystem(int gridheight, int gridwidth, float cellsize, Vector3 originPos)
+    {
+        _gridwidth = gridwidth;
+        _gridheight = gridheight;
+        _cellsize = cellsize;
+        _originPos = originPos;
+
+        _grid = new GridXZ<GridObject>(_gridwidth, _gridheight, _cellsize, _originPos, 
+            (GridXZ<GridObject> g, int x, int y)=>new GridObject(g, x, y));
+    }
+
+    public void CenterUpdate()
+    {
+        
+    }
+}
